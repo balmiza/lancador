aba19c891048924bf5ca96755d6ace7152b5e1e4
balmiza <51392688+balmiza@users.noreply.github.com>
2023-04-16 12:37:42 -0300

    versao 1.0.3

--- lancador/src/main/java/com/lancador/lancador/controller/LancadorController.java
+++ lancador/src/main/java/com/lancador/lancador/controller/LancadorController.java
@@ -1,17 +1,10 @@
 package com.lancador.lancador.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LancadorController {
-
-	@GetMapping("/lancar")
-	public String olaSpring() {
-		// exemple: curl -v http://localhost:8080/lancar
-		return "Olá Lancador";
-	}
 	
 	
 	@PatchMapping("ligarMotor")
