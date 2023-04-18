e36338a744495aa959b3712489339639cc697270
balmiza <51392688+balmiza@users.noreply.github.com>
2023-04-17 21:42:26 -0300

    versao 1.0.6

--- lancador/src/main/java/com/lancador/lancador/controller/LancadorController.java
+++ lancador/src/main/java/com/lancador/lancador/controller/LancadorController.java
@@ -1,6 +1,8 @@
 package com.lancador.lancador.controller;
 
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,10 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lancador.lancador.service.Application;
 import com.lancador.lancador.service.SimpleLed;
 import com.lancador.lancador.utils.PIN;
+import com.pi4j.Pi4J;
 import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.DigitalState;
+
 
 @RestController
 public class LancadorController implements Application{
+	
+	private static Scanner cmd;
 
 	@PatchMapping("ligarMotor")
 	public String ligarMotor() {
@@ -51,17 +58,51 @@ public class LancadorController implements Application{
 	
 	@PatchMapping("ligarLed2")
 	public void ligarLed2() throws IOException, InterruptedException {
-		// example: curl -v --request PATCH http://localhost:8080/ligarLed2
-		Runtime runtime = Runtime.getRuntime();
-		runtime.exec("gpio mode 4 out");
 		
-		for (int i = 0; i < 10; i++) {
-			runtime.exec("gpio write 4 1");
-			Thread.sleep(500);
-			runtime.exec("gpio write 4 0");
-			Thread.sleep(500);
-		}
-		//return "Motor ligado\n";
+		int DIGITAL_OUTPUT_PIN = 4;
+		// Initialize Pi4J with an auto context
+		// An auto context includes AUTO-DETECT BINDINGS enabled
+		// which will load all detected Pi4J extension libraries
+		// (Platforms and Providers) in the class path
+		var pi4j = Pi4J.newAutoContext();
+
+		// create a digital output instance using the default digital output provider
+		var output = pi4j.dout().create(DIGITAL_OUTPUT_PIN);
+		output.config().shutdownState(DigitalState.HIGH);
+
+		// setup a digital output listener to listen for any state changes on the digital output
+		output.addListener(System.out::println);
+
+		// lets invoke some changes on the digital output
+		output.state(DigitalState.HIGH)
+		          .state(DigitalState.LOW)
+		          .state(DigitalState.HIGH)
+		          .state(DigitalState.LOW);
+
+		// lets toggle the digital output state a few times
+		output.toggle()
+		          .toggle()
+		          .toggle();
+
+		// another friendly method of setting output state
+		output.high()
+		          .low();
+
+		// lets read the digital output state
+		System.out.print("CURRENT DIGITAL OUTPUT [" + output + "] STATE IS [");
+		System.out.println(output.state() + "]");
+
+		// pulse to HIGH state for 3 seconds
+		System.out.println("PULSING OUTPUT STATE TO HIGH FOR 3 SECONDS");
+		output.pulse(3, TimeUnit.SECONDS, DigitalState.HIGH);
+		System.out.println("PULSING OUTPUT STATE COMPLETE");
+
+		// shutdown Pi4J
+		pi4j.shutdown();
+		
+		
+		
+		
 	}
 
 	
